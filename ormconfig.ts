adfa4bb5422790f100a17085912224e76a93c97f
cqchien <caochientp1@gmail.com>
2021-04-22 16:40:21 +0700

    push heroku

--- ormconfig.ts
+++ ormconfig.ts
@@ -19,12 +19,15 @@ for (const envName of Object.keys(process.env)) {
 
 module.exports = {
     type: 'postgres',
-    url: process.env.DATABASE_URL,
-    host: process.env.POSTGRES_HOST,
-    port: +process.env.POSTGRES_PORT,
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
+    url: "postgres://mpgdahnnnwycit:e7493eb8c0aa1ecf3a276fa911e369f019ddea59f7d7efcb488abd573601dbdf@ec2-3-234-85-177.compute-1.amazonaws.com:5432/d7m22nin19d8c2",
+    ssl: {
+        rejectUnauthorized: false
+      },
+    // host: process.env.POSTGRES_HOST,
+    // port: +process.env.POSTGRES_PORT,
+    // username: process.env.POSTGRES_USERNAME,
+    // password: process.env.POSTGRES_PASSWORD,
+    // database: process.env.POSTGRES_DATABASE,
     namingStrategy: new SnakeNamingStrategy(),
     entities: ['src/modules/**/*.entity{.ts,.js}'],
     migrations: ['src/migrations/*{.ts,.js}'],
