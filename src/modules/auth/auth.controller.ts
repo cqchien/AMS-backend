d41a1640fdb524d7981f2cc7125402fa7d7d48b0
cqchien <caochientp1@gmail.com>
2021-04-26 11:05:21 +0700

    get current student login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,22 @@
+import { TeacherDto } from './../teacher/dto/TeacherDto';
+import { StudentDto } from './../student/dto/StudentDto';
+import { UserNotFoundException } from './../../exceptions/user-not-found.exception';
+import { TeacherEntity } from './../teacher/teacher.entity';
+import { StudentEntity } from './../student/student.entity';
+import { AuthUserInterceptor } from './../../interceptors/auth-user-interceptor.service';
+import { UseInterceptors } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UseGuards } from '@nestjs/common';
+import { Get } from '@nestjs/common';
 import { TokenPayloadDto } from './dto/TokenPayloadDto';
-import {
-    Body,
-    Controller,
-    HttpCode,
-    HttpStatus,
-    Post,
-} from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
 import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
 import { LoginPayloadDto } from './dto/LoginPayloadDto';
 import { UserLoginDto } from './dto/UserLoginDto';
+import { AuthUser } from '../../decorators/auth-user.decorator';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -21,7 +26,7 @@ export class AuthController {
         public readonly authService: AuthService,
     ) {}
 
-    // Teacher can only login with website 
+    // Teacher can only login with website
     // Student can only login with mobile app
     @Post('login')
     @HttpCode(HttpStatus.OK)
@@ -36,5 +41,19 @@ export class AuthController {
 
         const token = await this.authService.createToken(userEntity);
         return new LoginPayloadDto(token);
+    }
+
+    @Get('me')
+    @UseGuards(AuthGuard())
+    @UseInterceptors(AuthUserInterceptor)
+    @ApiBearerAuth()
+    @ApiOkResponse({ description: 'Get current user' })
+    getCurrentStudent(
+        @AuthUser() user: StudentEntity | TeacherEntity,
+    ): Promise<StudentDto | TeacherDto> {
+        if (!user) {
+            throw new UserNotFoundException();
+        }
+        return user.toDto();
     }
 }
