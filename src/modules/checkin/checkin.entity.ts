47434d899a921128fc7009bb2264642f5cf8df83
cqchien <caochientp1@gmail.com>
2021-05-07 17:13:26 +0700

    Iniialize checkin module and entity

---
+++ src/modules/checkin/checkin.entity.ts
@@ -0,0 +1,21 @@
+import { ClassEntity } from './../class/class.entity';
+import { CheckinDto } from './dto/checkinDto';
+import { StudentEntity } from './../student/student.entity';
+import { AbstractEntity } from '../../common/abstract.entity';
+import { Entity, JoinColumn, ManyToOne } from 'typeorm';
+
+@Entity({ name: 'checkin' })
+export class CheckinEntity extends AbstractEntity<CheckinDto> {
+    @ManyToOne(
+        () => StudentEntity,
+        (studentEntity) => studentEntity.checkin,
+    )
+    @JoinColumn({ name: 'student_id' })
+    student: StudentEntity;
+
+    @ManyToOne(() => ClassEntity, (classEntity) => classEntity)
+    @JoinColumn({ name: 'class_id' })
+    class: ClassEntity;
+
+    dtoClass = CheckinDto;
+}
