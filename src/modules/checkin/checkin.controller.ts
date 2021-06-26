83b1382b708c0cddf2438e8b638e3b35f92d944c
cqchien <caochientp1@gmail.com>
2021-06-26 19:21:22 +0700

    fix

--- src/modules/checkin/checkin.controller.ts
+++ src/modules/checkin/checkin.controller.ts
@@ -1,3 +1,5 @@
+import { PageOptionsDto } from './../../common/dto/PageOptionsDto';
+import { PageDto } from './../../common/dto/PageDto';
 import { CheckinPayloadDto } from './dto/checkinPayload';
 import { CheckinDto } from './dto/checkinDto';
 import { AuthUserInterceptor } from './../../interceptors/auth-user-interceptor.service';
@@ -10,10 +12,15 @@ import {
     UseInterceptors,
     HttpStatus,
     Body,
+    Get,
+    Query,
+    ValidationPipe,
+    Param,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
+import { UUIDParam } from 'decorators/http.decorators';
 
-@Controller('checkin')
+@Controller('/checkin')
 export class CheckinController {
     constructor(public checkinService: CheckinService) {}
 
@@ -34,4 +41,17 @@ export class CheckinController {
         );
         return checkinEntity.toDto();
     }
+
+    // @Get('/:id')
+    // @UseGuards(AuthGuard())
+    // @UseInterceptors(AuthUserInterceptor)
+    // @ApiBearerAuth()
+    // @ApiResponse({
+    //     status: HttpStatus.OK,
+    //     description: 'Show all Students is checked in with specific class',
+    // })
+    // async getAllCheckinWithClass(@Query(new ValidationPipe({transform: true})) pageOptionDto: PageOptionsDto, @UUIDParam('id') classId: string ): Promise<any>{
+    //     return this.checkinService.getAllCheckinWithClass(pageOptionDto, classId);
+    // }
+
 }
